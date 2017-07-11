6b20628a02754491b5017df7a30d8d9e497a52a0
enPiT-P13 <v072ff@yamaguchi-u.ac.jp>
2017-07-11 19:07:20 +0900

    今回の課題chapter4のじゃんけんの修正です。 お願いします

--- Janken/app/src/main/java/com/example/u90684/janken/MainActivity.java
+++ Janken/app/src/main/java/com/example/u90684/janken/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.u90684.janken;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
+
+        Intent intent = getIntent();
+        int id = intent.getIntExtra("MY_HAND", 0);
+        ImageView myHandImageView = (ImageView) findViewById(R.id.my_hand_image);
+    }
+
+    public void onJankenButtonTapped(View view) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("MY_HAND", view.getId());
+        startActivity(intent);
     }
 }
